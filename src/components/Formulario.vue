057f147da12c43feed67296613514b12b24796bb
José Junior <jzjuniorbrito@gmail.com>
2024-10-27 04:33:41 -0300

    commitando 4

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,40 +1,55 @@
 <template>
-   <div class="box"> 
-        <div class="columns">
-            <div class="columns is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
-            </div>
+    <div class="box formulario">
+      <div class="columns">
+        <div
+          class="column is-8"
+          role="form"
+          aria-label="Formulário para criação de uma nova tarefa"
+        >
+          <input
+            type="text"
+            class="input"
+            placeholder="Qual tarefa você deseja iniciar?"
+            v-model="descricao"
+          />
         </div>
         <div class="column">
-            <section>
-                <strong>00:00:00</strong>
-            </section>
-            <button class="button">
-                <span class="icon">
-                    <i class="fas fa-play"></i>
-                </span>
-                <span>play</span>
-            </button>
-            <button class="button">
-                <span class="icon">
-                    <i class="fas fa-stop"></i>
-                </span>
-                <span>stop</span>
-            </button>
+          <Temporizador @aoTemporizadorFinalizado="finalizarTarefa"/>
         </div>
-   </div>
-</template> 
+      </div>
+    </div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent } from "vue";
+  import Temporizador from "./Temporizador.vue";
+  
+  export default defineComponent({
+    name: "Formulario-Tracker",
+    emits: ['aoSalvarTarefa'],
+    components: {
+      Temporizador
+    },
+    data () {
+      return {
+        descricao: ''
+      }
+    },
+    methods: {
+      finalizarTarefa(tempoDecorrido: number) :  void {
+        this.$emit('aoSalvarTarefa', {
+          duracaoEmSegundos: tempoDecorrido,
+          descricao: this.descricao
+        })
+        this.descricao = ''
+      }
+    }
+  });
+  </script>
 
-<scrip lang="ts">
-
-    import { defineComponent }  from 'vue'
-
-    export default defineComponent({
-        name: 'Formulário'
-    })
-
-</script>
-
-<style scoped>
-
-</style>
+  <style>
+  .formulario {
+    color: var(--texto-primario);
+    background-color: var(--bg-primario);
+  }
+  </style>
